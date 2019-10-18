d753e4e5c99d9d6a1fb435a45214c93907a58c34
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2019-10-18 17:46:41 +0600

    Update weather_observation_4.sql

--- weather_observation_4.sql
+++ weather_observation_4.sql
@@ -1,1 +1,8 @@
+-- Let  be the number of CITY entries in STATION, and let  be the number of 
+--distinct CITY names in STATION; query the value of  from STATION. In other words,
+--find the difference between the total number of CITY entries in the table and the number 
+--of distinct CITY entries in the table.
+
+
+
 select count(CITY)- count(distinct CITY) from STATION
