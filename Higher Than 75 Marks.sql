258c8a755710323367e28716430b0322b200fc6f
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2019-10-19 00:26:20 +0600

    Create Higher Than 75 Marks.sql

---
+++ Higher Than 75 Marks.sql
@@ -0,0 +1,6 @@
+--Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+
+SELECT `name` FROM STUDENTS
+WHERE marks > 75
+ORDER BY SUBSTRING(`name`,-3) ASC,id ASC
