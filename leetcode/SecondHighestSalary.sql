0796a72bf2096d979ce3551c6d110391913bbca6
Moshiur Rahman <15215020+moshiurse@users.noreply.github.com>
2020-02-02 12:27:46 +0600

    Update SecondHighestSalary.sql

--- leetcode/SecondHighestSalary.sql
+++ leetcode/SecondHighestSalary.sql
@@ -3,3 +3,14 @@ ORDER BY Salary DESC
 LIMIT 2) a
 ORDER BY Salary ASC
 LIMIT 1
+-- this solution may cause error when there is no second salary
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+    
+    -- this is the solution for null 
